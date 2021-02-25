4216f9373c0b3611d2a4dbac1edd3c7259cfcec1
Marcella Melo <marcellamelo2015@gmail.com>
2021-02-25 11:37:33 +0000

    5tentativa

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -4,7 +4,7 @@ import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.util.*
 
-abstract class Veiculo (id:String) : Movimentavel {
+open abstract class Veiculo (id:String) : Movimentavel {
 
     var identificador = id
     lateinit var posicao: Posicao
