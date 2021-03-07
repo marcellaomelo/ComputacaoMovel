025ec6489a69c28a0760655b59c9bd07174dcf5d
Marcella Melo <marcellamelo2015@gmail.com>
2021-03-07 16:01:15 +0000

    falha boolean

--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -61,12 +61,11 @@ data class Pessoa (var nome: String, val dataDeNascimento: Date): Movimentavel{
     }
 
     fun temCarta(): Boolean{
-        if(carta == null){
+        if(carta != null){
             return false
         }
         return true
     }
-    @Throws(MenorDeIdadeException::class)
     fun tirarCarta(){
         var dataAtual: Date = Date()
         if(dataAtual.year - dataDeNascimento.year < 18){
